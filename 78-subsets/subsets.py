9d6dece1a17b1636f92a2c65ceb4387207a0105a
varshini-raghavan <93823443+varshini-raghavan@users.noreply.github.com>
2024-05-26 13:29:27 +0530

    Time: 35 ms (72.59%) | Memory: 16.7 MB (78.60%) - LeetSync

---
+++ 78-subsets/subsets.py
@@ -0,0 +1,22 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        def solve(n:int,arr:List[int],check:List[int],i:int,ans:List[List[int]]):
+            if i==n:
+                l=[]
+                for j in range(n):
+                    if check[j]!=-1:
+                        l.append(arr[j])
+                ans.append(l)
+            else:
+                check[i]=1
+                solve(n,arr,check,i+1,ans)
+                check[i]=-1
+                solve(n,arr,check,i+1,ans)
+            
+        check=[]
+        ans=[]
+        n=len(nums)
+        for i in range(n):
+            check.append(-1)
+        solve(n,nums,check,0,ans)
+        return ans
