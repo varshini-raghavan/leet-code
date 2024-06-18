9a4671b06bd227a1d363dc17ee9b141ad8a28cc7
varshini-raghavan <93823443+varshini-raghavan@users.noreply.github.com>
2024-06-18 08:41:18 +0530

    Time: 303 ms (68.96%) | Memory: 17.9 MB (76.80%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
@@ -1,23 +1,23 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-        ROWS, COLS = len(matrix), len(matrix[0])
-        dp = {}  # (r, c) -> LIP
+        if not matrix or not matrix[0]:
+            return 0
+        rows, cols = len(matrix), len(matrix[0])
+        dp =[[0] * cols for i in range(rows)]
+        def dfs(i, j):
+            if not dp[i][j]:
+                val = matrix[i][j]
+                dp[i][j] = 1 + max(
+                    dfs(i - 1, j) if i and val > matrix[i - 1][j] else 0,
+                    dfs(i + 1, j) if i < rows - 1 and val > matrix[i + 1][j] else 0,
+                    dfs(i, j - 1) if j and val > matrix[i][j - 1] else 0,
+                    dfs(i, j + 1) if j < cols - 1 and val > matrix[i][j + 1] else 0)
+            return dp[i][j]
+        
+        for r in range(rows):
+            for c in range(cols):
+                dfs(r,c)
+        return max(max(x) for x in dp)
 
-        def dfs(r, c, prevVal):
-            if r < 0 or r == ROWS or c < 0 or c == COLS or matrix[r][c] <= prevVal:
-                return 0
-            if (r, c) in dp:
-                return dp[(r, c)]
 
-            res = 1
-            res = max(res, 1 + dfs(r + 1, c, matrix[r][c]))
-            res = max(res, 1 + dfs(r - 1, c, matrix[r][c]))
-            res = max(res, 1 + dfs(r, c + 1, matrix[r][c]))
-            res = max(res, 1 + dfs(r, c - 1, matrix[r][c]))
-            dp[(r, c)] = res
-            return res
-
-        for r in range(ROWS):
-            for c in range(COLS):
-                dfs(r, c, -1)
-        return max(dp.values())
+                
